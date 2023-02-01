85c3ccfb8c6d365a5f6cc150d97919b45ee62a14
lizard20 <anunez20@gmail.com>
2023-02-01 12:06:41 -0600

    odd_or_even.c added

---
+++ odd_or_even.c
@@ -0,0 +1,22 @@
+/*
+** Determines if a number is odd or even
+** Author: Aldo Núñez
+** */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int
+main (int argc, char* argv[])
+{
+	if (argc < 2)
+	{
+		printf("Usage: ./odd_or_even n\n");
+		return -1;
+	}
+
+	int num = atoi(argv[1]);
+	printf("%s\n", num % 2 ? "Odd" : "Even");
+
+	return 0;
+}
