387eb0f998316e6a317aabb676b29312e8e0403b
lizard20 <anunez20@gmail.com>
2023-02-05 10:14:13 -0600

    sorting_by_selection.c added

---
+++ sorting_by_selection.c
@@ -0,0 +1,72 @@
+/*
+**	Sorting using the selection
+**	algorithm.
+**	Based on: Fundamental of algorithmics.
+**	G. Brassard and P. Bratley
+**	page 62.
+**
+**	Author: Aldo Núñez
+** */
+
+
+
+#include <stdio.h>
+
+void selection(int*);
+void print(int*);
+
+unsigned N;
+
+int
+main()
+{
+
+	// Testing data
+	// int u[] = {1,2,3,4,5,6};
+	int u[] = {6,5,4,3,2,1};
+	
+	// size of the array 
+	N = sizeof(u)/sizeof(int);
+
+	print(u);
+	printf("-------------------------\n");
+	printf("-------------------------\n");
+	selection(u);
+	print(u);
+
+	return 0;
+}
+
+void 
+selection(int* t)
+{
+	int minj, minx;
+
+	for (int i = 0; i < N - 1; ++i )
+	{
+		minj = i;
+		minx = t[i];
+		for ( int j = i + 1; j < N; ++j)
+		{
+			if (t[j] < minx )
+			{
+				minj = j;
+				minx = t[j];
+			}
+			t[minj] = t[i];
+			t[i] = minx;
+			print(t);
+		}
+		printf("-------------------------\n");
+	}
+}
+
+void 
+print(int* u)
+{
+	for (int i = 0; i < N; ++i)
+	{
+		printf("%d ", u[i]);
+	}
+	printf("\n");
+}
