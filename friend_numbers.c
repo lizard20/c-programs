7297772f0b34035fd9e7c745d3e113f35a7d43a9
lizard20 <anunez20@gmail.com>
2023-03-15 11:13:47 -0600

    update friend_numbers.c

--- friend_numbers.c
+++ friend_numbers.c
@@ -3,17 +3,22 @@
 **	if they are friends. Is so, prints the 
 **	proper divisor numbers.
 **
-** Test numbers:
-  (220, 284),
-  (1184, 1210),
-  (2620, 2924), 
-  (5020, 5564),
-  (6232, 6368),
-  (10744, 10856), 
-  (12285, 14595), 
-  (17296, 18416),
-  (63020, 76084),
-  (66928, 66992)
+**	Amicable or friend numbers, are two different
+**	natural numbers related in such a way that
+**	the sum of the proper divisors of each 
+**	is equal to the other number.
+**
+** Friends numbers:
+	(220, 284),
+  	(1184, 1210),
+  	(2620, 2924), 
+  	(5020, 5564),
+  	(6232, 6368),
+  	(10744, 10856), 
+  	(12285, 14595), 
+  	(17296, 18416),
+  	(63020, 76084),
+  	(66928, 66992)
 	Author: Aldo Núñez
 ** */
 
