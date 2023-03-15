995e5fcdb883670f5752d8b433a9a8be89a7a54d
lizard20 <anunez20@gmail.com>
2023-03-15 11:40:10 -0600

    friend_numbers_2.c update

--- friend_numbers_2.c
+++ friend_numbers_2.c
@@ -3,17 +3,23 @@
 **	if they are friends. Is so, prints the 
 **	proper divisor numbers.
 **
-** Test numbers:
+**	Amicable or friend numbers, are two different
+**	natural numbers related in such a way that
+**	the sum of the proper divisors of each 
+**	is equal to the other number.
+**
+** Friends numbers:
 	(220, 284),
   	(1184, 1210),
-	(2620, 2924), 
-	(5020, 5564),
-	(6232, 6368),
-	(10744, 10856), 
-	(12285, 14595), 
-	(17296, 18416),
-	(63020, 76084),
-	(66928, 66992)
+  	(2620, 2924), 
+  	(5020, 5564),
+  	(6232, 6368),
+  	(10744, 10856), 
+  	(12285, 14595), 
+  	(17296, 18416),
+  	(63020, 76084),
+  	(66928, 66992)
+
 	Author: Aldo Núñez
 ** */
 
@@ -21,7 +27,7 @@
 #include <stdlib.h>
 
 // prototypes
-int add_proper_divisors(int);
+int add_proper_divisors(const int);
 void print_proper_divisors(int);
 
 int
@@ -35,11 +41,11 @@ main(int argc, char* argv[])
 	i = add_proper_divisors(a);
 	if ((i == b) && ((j = add_proper_divisors(b)) == a))
 	{
-		printf("%d and %d are friends numbers\n", a, b);
+		printf("%d and %d are friend numbers\n", a, b);
 	}
 	else
 	{
-		printf("%d and %d are NOT friends numbers\n", a, b);
+		printf("%d and %d are NOT friend numbers\n", a, b);
 	}
 	printf("The proper divisors are:\n");
 	printf("%d: ",a);
@@ -51,7 +57,7 @@ main(int argc, char* argv[])
 }
 
 int
-add_proper_divisors(int n)
+add_proper_divisors(const int n)
 {
 	int sum = 0;
 
