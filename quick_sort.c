90af3d0846be3db4be23858043ec4125c089ce22
lizard20 <anunez20@gmail.com>
2024-09-27 07:25:20 -0600

    quick_sort.c updated

--- quick_sort.c
+++ quick_sort.c
@@ -1,14 +1,22 @@
 /*
 ** Sorting numbers using the
 ** quick sort algorithm
-**
+** 
+** For integer numbers
+** Uncomment lines: 18, 28, 99 
+** Comment lines:   19, 31, 100
+** 
+** For floating point numbers 
+** Uncomment lines: 19, 31, 100
+** Comment lines:   18, 28, 99
+
 ** Author: Aldo Núñez
 ** */
 
 #include <stdio.h>
 
 //typedef int DataType; 	// integer numbers
-typedef double DataType;	// floating numbers
+typedef double DataType;	// floating point numbers
 
 void quick_sort(DataType[], int, int);
 void print_array (const DataType*, const size_t);
@@ -19,7 +27,7 @@ main()
 	// Test for integers numbers
 	// DataType a[] = {250, 129, -89, -45, 34, 29, 134, 223};
 
-	// Test for floating numbers
+	// Test for floating point numbers
 	DataType a[] = {4.5, -45.0, 29.6, 78.6, 10.5, 6.9, 134.5, 245.45};
 
 	const size_t N =  sizeof(a)/sizeof(DataType);
@@ -89,7 +97,7 @@ print_array(const DataType* a, const size_t N)
 	for (unsigned int i = 0; i < N; ++i)
 	{
 	//	printf("%d ", a[i]);	// print integer numbers
-		printf("%.3f ", a[i]);	// print floating numbers
+		printf("%.1f ", a[i]);	// print floating point numbers
 	}
 	putchar('\n');
 }
